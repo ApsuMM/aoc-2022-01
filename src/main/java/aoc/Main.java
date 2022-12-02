689d5adfa6a91d08570786476fd668e84f53998a
Magnus <inaction.tk@gmail.com>
2022-12-02 20:26:50 +0100

    sums up top most elfs

--- src/main/java/aoc/Main.java
+++ src/main/java/aoc/Main.java
@@ -7,12 +7,17 @@ import java.io.FileReader;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.Reader;
+import java.util.ArrayList;
+import java.util.Comparator;
+import java.util.List;
 
 public class Main {
 
 	public static void main(String[] args) throws IOException {
 		// TODO Auto-generated method stub
 		BufferedReader reader = new BufferedReader(new FileReader(Main.class.getClassLoader().getResource("data.txt").getFile()));
+		List<Integer> elfs = new ArrayList<Integer>();
+		
 		int finalElf = 0;
 		int currentElf = 0;
 		
@@ -20,12 +25,19 @@ public class Main {
 		while ((line = reader.readLine()) != null) {
 			if(line.isEmpty())
 			{
-				finalElf = finalElf < currentElf ? currentElf : finalElf;
+				elfs.add(currentElf);
 				currentElf = 0;
 				continue;
 			}
 			currentElf += Integer.valueOf(line);
 		}
+		elfs.sort(Comparator.reverseOrder());
+		
+		for (int i = 0; i < 3; i++)
+		{
+			finalElf += elfs.get(i);
+		}
+
 		System.out.println(finalElf);
 	}
 }
