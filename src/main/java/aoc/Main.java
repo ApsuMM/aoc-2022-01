89d523ed47a8dfb9f3ee35d2c4c3082f21bd7521
Magnus <inaction.tk@gmail.com>
2022-12-02 20:43:18 +0100

    Revert to 56e05fced

--- src/main/java/aoc/Main.java
+++ src/main/java/aoc/Main.java
@@ -7,17 +7,12 @@ import java.io.FileReader;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.Reader;
-import java.util.ArrayList;
-import java.util.Comparator;
-import java.util.List;
 
 public class Main {
 
 	public static void main(String[] args) throws IOException {
 		// TODO Auto-generated method stub
 		BufferedReader reader = new BufferedReader(new FileReader(Main.class.getClassLoader().getResource("data.txt").getFile()));
-		List<Integer> elfs = new ArrayList<Integer>();
-		
 		int finalElf = 0;
 		int currentElf = 0;
 		
@@ -25,19 +20,12 @@ public class Main {
 		while ((line = reader.readLine()) != null) {
 			if(line.isEmpty())
 			{
-				elfs.add(currentElf);
+				finalElf = finalElf < currentElf ? currentElf : finalElf;
 				currentElf = 0;
 				continue;
 			}
 			currentElf += Integer.valueOf(line);
 		}
-		elfs.sort(Comparator.reverseOrder());
-		
-		for (int i = 0; i < 3; i++)
-		{
-			finalElf += elfs.get(i);
-		}
-
 		System.out.println(finalElf);
 	}
 }
